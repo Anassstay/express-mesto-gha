a36a6679048570dcb9b8270644544135c17e695b
A.nassstay <Anastasia.stay@yandex.ru>
2023-05-11 13:03:52 +0300

    14/3

--- routes/users.js
+++ routes/users.js
@@ -4,13 +4,13 @@ const userRouter = require('express').Router();
 // импорт списков
 const {
   getUsers,
-  getUserId,
+  getUser,
   updateUserInfo,
   updateUserAvatar,
 } = require('../controllers/users');
 
 userRouter.get('/users', getUsers);
-userRouter.get('/users/:userId', getUserId);
+userRouter.get('/users/:userId', getUser);
 userRouter.patch('/users/me', updateUserInfo);
 userRouter.patch('/users/me/avatar', updateUserAvatar);
 
